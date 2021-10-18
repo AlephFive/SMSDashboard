c4e3e3fe0aec956b9cdebff2ef3712e35d1eae6c
Shao-en Ma <spo2.spire.club@gmail.com>
2021-10-17 20:27:27 -0400

    Can now get stuff from firebase

--- smsdash/src/App.js
+++ smsdash/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://nothatbrian.pythonanywhere.com/dashboard")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result["results"]);
+          setEntries(result["results"]);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log("ERROR Fetch failed");
+          setEntries([]);
+        }
+      )
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <body>
+        {entries.map((obj, index) => (
+          <p>{JSON.stringify(obj)}</p>
+        ))}
+      </body>
     </div>
   );
 }
